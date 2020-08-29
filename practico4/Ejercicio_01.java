d4e0b4b51ed9554541b623a4ef8976089a6971fd
yo <ycedeno363@gmail.com>
2020-08-29 11:25:38 -0400

    cambios varios

---
+++ practico4/Ejercicio_01.java
@@ -0,0 +1,12 @@
+package Clase02;
+
+public class Ejercicio_01 {
+
+//    1)	Mostrar “Holamundo” en pantalla
+//    INICIO
+//    MOSTRAR "HolaMundo";
+//    FIN
+    public static void main (String args []){
+        System.out.println("Hola mundo");
+    }
+}
